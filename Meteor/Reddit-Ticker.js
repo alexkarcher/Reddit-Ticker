51a3a927068e8986552a24f14be7d4b612e416c7
Alex Karcher <me@alexkarcher.com>
2015-11-18 03:30:23 -0500

    Reddit data pulling working
    This might look like I just added 30 lines of code, but it represents so much pain and suffering. Look at the amount of nested objects in the bastard dressed in json that reddit returns. There has to be an easier way!

--- Meteor/Reddit-Ticker.js
+++ Meteor/Reddit-Ticker.js
@@ -1,6 +1,19 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  
+  Meteor.call('reddit', function (err, data){
+    
+    //Debug print out the entire object returned from Reddit
+    console.log(data);
+    
+    /*Seriously Reddit? This is what it apparently takes 
+       to get at the post title and upvote count */
+    for (var i in data.data.data.children){
+      console.log({title: data.data.data.children[i].data.title, 
+                   votes: data.data.data.children[i].data.score});
+    }
+  });  
 
   Template.hello.helpers({
     counter: function () {
@@ -19,5 +32,15 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    
   });
 }
+
+Meteor.methods({
+  reddit: function () {
+    var result = HTTP.call("GET", "http://reddit.com/r/rpi/top.json",
+                           {params: {limit: 5, t: "year"}});
+    return result;
+
+  }
+});
